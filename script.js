b1b25ce4314f2c1b75fdaec0bbd07890eefe903a
Luminokinono <bilaloumidxv3@gmail.com>
2025-04-17 19:43:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-document.querySelectorAll('nav a').forEach(link => {
-    link.addEventListener('click', function(e){
-        e.preventDefault();
-        const target = document.querySelector(this.getAttribute('href'));
-        window.scrollTo({
-            top: target.offsetTop,
-            behavior: 'smooth'
-        });
+// Scroll fluide pour les liens internes
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
     });
+  });
 });
